cefd95f05cb6a0ea8a0436035f45750667653984
rawatria <91248977+rawatria@users.noreply.github.com>
2021-09-23 20:32:27 +0530

    Update linearSearch.cpp

--- week0/linearSearch.cpp
+++ week0/linearSearch.cpp
@@ -1,1 +1,33 @@
-
+#include<iostream>
+#include<vector>
+using namespace std;
+int LinearSearch(vector<int>&a, int k){
+    int n = a.size(), index = -1;
+    bool f = false;
+    for(int i=0; i<=n; i++){
+        if(a[i] == k){
+            index = i;
+            f = true;
+        }
+    }
+    if(f){
+        cout<<"founded at index: " << index << endl;
+    }
+    else cout << "not founded" << endl;
+    return 0;
+    
+}
+int main(){
+    int i,k,n;
+    cout<<"Enter n";
+    cin>>n;
+    vector<int>a(n);
+    cout<<"Enter elements: ";
+    for(i=1;i<=n;i++){
+        cin>>a[i];
+    }
+    cout<<"Enter the value you want to search: ";
+    cin>>k;
+    LinearSearch(a,k);
+    
+}
